326c68ea50534a76ff6f307d6792607b1cb91430
Michael G Nielsen <mgn@mgnielsen.dk>
2026-04-11 11:48:47 +0200

    added gemma4 models

--- pull_models.sh
+++ pull_models.sh
@@ -3,3 +3,5 @@ docker exec -it ollama ollama pull gemma2:9b   && sleep 10
 docker exec -it ollama ollama pull phi3        && sleep 10
 docker exec -it ollama ollama pull llama3.2:3b && sleep 10
 docker exec -it ollama ollama pull llama3.1:8b && sleep 10
+docker exec -it ollama ollama pull gemma4:e2b  && sleep 10
+docker exec -it ollama ollama pull gemma4:e4b  && sleep 10
