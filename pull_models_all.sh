57b1bcb090c4264977a1c8bf58fbfedbea9f9c6d
Michael G. Nielsen <mgn@mgnielsen.dk>
2026-05-06 13:16:08 +0200

    fixed +x on bash scripts

--- pull_models_all.sh
+++ pull_models_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./ pull_models_llama.sh
-./ pull_models_deepseek.sh
-./ pull_models_phi.sh
-./ pull_models_gemma.sh
-./ pull_models_qwen.sh
+./pull_models_llama.sh
+./pull_models_deepseek.sh
+./pull_models_phi.sh
+./pull_models_gemma.sh
+./pull_models_qwen.sh
